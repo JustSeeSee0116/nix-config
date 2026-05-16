7726d99ad18aee9b7917bf962bcfa2cc92bd7854
JustSeeSee0116 <pengshen55@gmail.com>
2026-05-16 19:59:56 +0800

    minor change

--- configuration.nix
+++ configuration.nix
@@ -196,7 +196,7 @@
     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #wget
     kitty#alacritty # 
-    #fuzzel
+    fuzzel
     #hyprlock #swaylock
     #mako
     #swayidle
