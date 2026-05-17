dc802acf5c247b6c9c2cf8c80472d9bc445b97ca
JustSeeSee0116 <pengshen55@gmail.com>
2026-05-17 14:03:22 +0800

    fix

--- modules/hosts/nixos/default.nix
+++ modules/hosts/nixos/default.nix
@@ -1,7 +1,7 @@
 { self, inputs, ... }: {
 
   # This is your system configuration entry-point
-  flake.nixosConfigurations.myMachine = inputs.nixpkgs.lib.nixosSystem {
+  flake.nixosConfigurations.nixos = inputs.nixpkgs.lib.nixosSystem {
     #system = "x86_64-linux";
     modules = [
       self.nixosModules.myMachineConfiguration
