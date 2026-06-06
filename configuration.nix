375c2bb76dd1456798d57707c1531b0095b34074
JustSeeSee0116 <pengshen55@gmail.com>
2026-06-06 10:48:17 +0800

    fix

--- configuration.nix
+++ configuration.nix
@@ -92,17 +92,19 @@
     hideMounts = true;
     directories = [
       "/var/log"
-      "/var/lib/bluetooth"
+      #"/var/lib/bluetooth"
       "/var/lib/nixos"
-      "/var/lib/systemd/coredump"
+      #"/var/lib/systemd/coredump"
       "/etc/NetworkManager/system-connections"
-      "/etc/nixos"
-      { directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
+      #"/etc/nixos"
+      #{ directory = "/var/lib/colord"; user = "colord"; group = "colord"; mode = "u=rwx,g=rx,o="; }
       "/home"
     ];
     files = [
       "/etc/machine-id"
-      { file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
+      "/etc/passwd"
+      "/etc/shadow"
+      #{ file = "/var/keys/secret_file"; parentDirectory = { mode = "u=rwx,g=,o="; }; }
     ];
     users.ss = {
       directories = [
@@ -111,15 +113,15 @@
         "Pictures"
         "Documents"
         "Videos"
-        "VirtualBox VMs"
-        { directory = ".gnupg"; mode = "0700"; }
-        { directory = ".ssh"; mode = "0700"; }
-        { directory = ".nixops"; mode = "0700"; }
-        { directory = ".local/share/keyrings"; mode = "0700"; }
-        ".local/share/direnv"
+        #"VirtualBox VMs"
+        #{ directory = ".gnupg"; mode = "0700"; }
+        #{ directory = ".ssh"; mode = "0700"; }
+        #{ directory = ".nixops"; mode = "0700"; }
+        #{ directory = ".local/share/keyrings"; mode = "0700"; }
+        #".local/share/direnv"
       ];
       files = [
-        ".screenrc"
+        #".screenrc"
       ];
     };
   };
